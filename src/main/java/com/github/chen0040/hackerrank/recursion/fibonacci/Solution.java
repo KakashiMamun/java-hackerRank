391ea1acdfd46203ffa86698c4625b62f9a78ae7
xschen <xs0040@gmail.com>
2017-08-09 20:52:58 +0800

    fibonacci

---
+++ src/main/java/com/github/chen0040/hackerrank/recursion/fibonacci/Solution.java
@@ -0,0 +1,23 @@
+package com.github.chen0040.hackerrank.recursion.fibonacci;
+
+
+/**
+ * Created by xschen on 9/8/2017.
+ */
+import java.util.*;
+
+public class Solution {
+
+   public static int fibonacci(int n) {
+      if(n <= 1) return n;
+      return fibonacci(n-1) + fibonacci(n-2);
+      // Complete the function.
+   }
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt();
+      scanner.close();
+      System.out.println(fibonacci(n));
+   }
+}
